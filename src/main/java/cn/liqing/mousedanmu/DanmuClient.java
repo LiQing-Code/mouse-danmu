f75cbaabfdacd6cd913859fb01eddf21a680a635
立青 <oh-0.0@outlook.com>
2023-01-10 12:22:32 +0800

    修改用户uid数据类型为long 修改sc显示逻辑

--- src/main/java/cn/liqing/mousedanmu/DanmuClient.java
+++ src/main/java/cn/liqing/mousedanmu/DanmuClient.java
@@ -6,21 +6,15 @@ import cn.liqing.mousedanmu.config.ModConfig;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.BossBarHud;
-import net.minecraft.client.gui.hud.ClientBossBar;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.entity.boss.BossBar;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.packet.s2c.play.BossBarS2CPacket;
-import net.minecraft.text.*;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.UUID;
 
 import static net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents.Disconnect;
 import static net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents.Join;
@@ -30,6 +24,7 @@ public class DanmuClient extends BLiveClient implements Disconnect, Join {
     final ModConfig config;
     final DanmuConverter danmuText;
     final DanmuColorPicker colorPicker;
+    final SuperChatManager superChatManager;
 
     public DanmuClient(ModConfig config) {
         ClientPlayConnectionEvents.JOIN.register(this);
@@ -37,6 +32,7 @@ public class DanmuClient extends BLiveClient implements Disconnect, Join {
         this.config = config;
         colorPicker = new DanmuColorPicker(config);
         danmuText = new DanmuConverter(config, colorPicker);
+        superChatManager = new SuperChatManager(danmuText, colorPicker);
     }
 
     @Override
@@ -77,10 +73,7 @@ public class DanmuClient extends BLiveClient implements Disconnect, Join {
             status();
             return false;
         }
-        for (int i = bossBars.size() - 1; i >= 0; i--) {
-            delSuperChat(bossBars.get(i).getUuid());
-        }
-        bossBars.clear();
+        superChatManager.clear();
         return true;
     }
 
@@ -122,7 +115,7 @@ public class DanmuClient extends BLiveClient implements Disconnect, Join {
         //表情
         var emoji = new Emoji();
         emoji.uri = "https://www.bilibili.com/video/BV1GJ411x7h7";
-        emoji.body = "这是表情，但没法(至少我不会)显示图片，可以点击查看图片！";
+        emoji.body = "这是表情，我不会显示图片，可以点击查看图片！";
         emoji.user = user;
         onEmoji(emoji);
         Thread.sleep(delay);
@@ -224,21 +217,28 @@ public class DanmuClient extends BLiveClient implements Disconnect, Join {
         Thread.sleep(delay);
         user.fansMedal.name = "蒙古人";
         user.fansMedal.level = 29;
-        danmu.body = "别激动，这其实是后台代码生成的总督，希望大家别被骗了";
+        danmu.body = "别激动，这其实是代码生成的总督，希望大家别被骗了";
         onDanmu(danmu);
         user.fansMedal.name = "粉丝团";
         Thread.sleep(delay);
 
         var sc = new SuperChat();
         sc.user = user;
-        sc.time = 10;
+
+        sc.time = 30;
+        sc.price = 1000;
+        sc.body = "我柜子动了，我不玩了";
+        onSuperChat(sc);
+        Thread.sleep(delay);
+
+        sc.time = 20;
         sc.price = 30;
-        sc.body = "sc才能点歌吗";
+        sc.body = "SC才能点歌吗";
         onSuperChat(sc);
         Thread.sleep(delay);
 
         sc.price = 50;
-        sc.body = "这不对吧,进度怎么走这么快";
+        sc.body = "这不对吧,SC时间怎么走这么快";
         onSuperChat(sc);
         Thread.sleep(delay);
 
@@ -252,11 +252,7 @@ public class DanmuClient extends BLiveClient implements Disconnect, Join {
         onSuperChat(sc);
         Thread.sleep(delay);
 
-        sc.price = 1000;
-        sc.body = "我柜子动力我不玩了";
-        onSuperChat(sc);
-        Thread.sleep(delay);
-
+        sc.time = 30;
         user.fansMedal.name = "雏草姬";
         sc.price = 2000;
         sc.body = "关注永雏塔菲喵！关注永雏塔菲谢谢喵！";
@@ -290,7 +286,7 @@ public class DanmuClient extends BLiveClient implements Disconnect, Join {
     @Override
     public void onSuperChat(SuperChat sc) {
         if (config.superChat.isShowBossBar) {
-            addSuperChat(sc);
+            superChatManager.add(sc);
         }
         if (config.superChat.isShowChat && player != null) {
             player.sendMessage(danmuText.convert(sc));
@@ -313,87 +309,11 @@ public class DanmuClient extends BLiveClient implements Disconnect, Join {
                     danmuText.convert(interactive), false);
     }
 
-    ArrayList<BossBar> bossBars = new ArrayList<>();
-
-    @SuppressWarnings("all")
-    public UUID addSuperChat(@NotNull SuperChat sc) {
-        UUID uuid = UUID.randomUUID();
-        BossBarHud hud = MinecraftClient.getInstance().inGameHud.getBossBarHud();
-        var fans = sc.user.fansMedal;
-        sc.user.fansMedal = null;
-        MutableText text = danmuText
-                .convertUser(sc.user)
-                .append(Texts.bracketed(Text.literal(String.valueOf(sc.price)))
-                        .styled(style -> style.withColor(colorPicker.superChat(sc.price))))
-                .append(Text.literal(sc.body).formatted(Formatting.WHITE));
-        ClientBossBar bossBar = new ClientBossBar(
-                uuid, text, 1,
-                colorPicker.superChatBossBar(sc.price),
-                BossBar.Style.PROGRESS,
-                false, false, false);
-        sc.user.fansMedal = fans;
-
-        bossBars.add(bossBar);
-        BossBarS2CPacket packet = BossBarS2CPacket.add(bossBar);
-        MinecraftClient.getInstance().submit(() -> {
-            //处理溢出BossBar
-            if (bossBars.size() > 4) {
-                for (int i = bossBars.size() - 1; i > 0; i--) {
-                    if (bossBars.get(i).getPercent() <= 0)
-                        bossBars.remove(i);
-                }
-                if (bossBars.size() > 0) {
-                    hud.handlePacket(BossBarS2CPacket.remove(bossBars.get(0).getUuid()));
-                    bossBars.remove(0);
-                }
-            }
-            hud.handlePacket(packet);
-        });
-
-        new Timer().schedule(new TimerTask() {
-            float x = 1f / sc.time;
-            float percent = 1f;
-
-            @Override
-            public void run() {
-                percent -= x;
-                bossBar.setPercent(percent);
-                BossBarS2CPacket p;
-                if (percent <= 0) {
-                    p = BossBarS2CPacket.remove(uuid);
-                    this.cancel();
-                } else {
-                    p = BossBarS2CPacket.updateProgress(bossBar);
-                }
-                MinecraftClient.getInstance().submit(() -> hud.handlePacket(p));
-            }
-        }, 0, 1000);
-        return uuid;
-    }
-
-    @SuppressWarnings("unused")
-    public void delSuperChat(UUID uuid) {
-        bossBars.stream().filter(b -> b.getUuid() == uuid)
-                .findAny().ifPresent(bossBar -> bossBars.remove(bossBar));
-        BossBarS2CPacket packet = BossBarS2CPacket.remove(uuid);
-        MinecraftClient.getInstance().submit(() ->
-                MinecraftClient.getInstance().inGameHud.getBossBarHud().handlePacket(packet));
-    }
-
-    int retries = 0;
     @Override
     public void onClose(int code, String reason, boolean remote) {
         status();
-        if (!remote)
-            return;
         String msg;
-        if (retries < 4) {
-            connect(room);
-            retries++;
-            msg = "已断线重连，代码：%d".formatted(code);
-        } else {
-            msg = "已断开连接，代码：%d".formatted(code);
-        }
+        msg = "已断开连接，代码：%d".formatted(code);
         MouseDanmu.LOGGER.info(msg);
         if (player != null)
             player.sendMessage(Text.literal(msg).formatted(Formatting.GOLD));
@@ -410,7 +330,6 @@ public class DanmuClient extends BLiveClient implements Disconnect, Join {
     @Override
     public void onOpen() {
         MouseDanmu.LOGGER.info("已连接：%d".formatted(room));
-        retries = 0;
         status();
     }
 }
